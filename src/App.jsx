9330265fe25e0a71e8f76655f21facd915b8e9d3
Remi Olateru <remiolateru@gmail.com>
2024-10-17 07:01:16 -0400

    added data.js, core components array to put into props. destructured props

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,73 @@
+
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import configImg from './assets/config.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description}  React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
+// object destructoring can be used to target the incoming properties of the object
+function CoreConcepts({title, description,image}) {
 
+  return <li>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The Core UI Building block"
+              image={componentsImg} />
+
+            {/* ... the spread operator pulls out each element within an array or object */}
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+
+            <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+
+
+        </section>
+
+
       </main>
     </div>
   );
