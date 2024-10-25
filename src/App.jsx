c8cf555c1409911a26cb44c5abf7c2e26246f07e
Remi Olateru <remiolateru@gmail.com>
2024-10-24 22:33:19 -0400

    refactoring the code to make it more readable and modular

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
-import {useState, Fragment} from 'react';
+import { Fragment} from 'react';
 import componentsImg from './assets/components.png';
 import configImg from './assets/config.png';
-import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcepts from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 
 
@@ -15,63 +14,21 @@ import { EXAMPLES } from './data.js';
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState(null);
+ 
 
 
-
-  function handleSelect(selectedButton) {
-      setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = (<p>Please select a topic</p>);
-
-  if(selectedTopic){
-    tabContent =  
-    <div id="tab-content">
-           
-           <h3>{EXAMPLES[selectedTopic].title}</h3>
-           <p>{EXAMPLES[selectedTopic].description}</p>
-           <pre>
-             <code>
-             {EXAMPLES[selectedTopic].code }
-             </code>
-           </pre>
-
-         </div>
-  }
 
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
+        <CoreConcepts/>
 
-          <ul>
-           {CORE_CONCEPTS.map((conceptItems) => <CoreConcepts {...conceptItems} />)}
-          </ul>
-        </section>
- 
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton  isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-
-          {tabContent}
-          
+        <Examples/>
       
-
-
-        </section>
-
-
-      </main>
-    </Fragment>
+      </main>  
+    </>
   );
 }
 
--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,9 +1,16 @@
-// object destructoring can be used to target the incoming properties of the object
-export default function CoreConcepts({ title, description, image }) {
+import CoreConcept from "./CoreConcept";
+import { CORE_CONCEPTS } from "../data";
+export default function CoreConcepts(){
 
-    return <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  }
+return (
+    <section id="core-concepts">
+          <h2>Core Concepts</h2>
+
+          <ul>
+           {CORE_CONCEPTS.map((conceptItems) => <CoreConcept {...conceptItems} />)}
+          </ul>
+        </section>
+ 
+);
+
+}
